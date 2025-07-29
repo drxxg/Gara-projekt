5df74bdcd82bed09496d91e19cb8dd4cf940acbb
Kacper Roszczyna <kacper.roszczyna@gmail.com>
2025-07-29 18:14:42 +0200

    Zadanie 6

--- src/garage/Car.java
+++ src/garage/Car.java
@@ -2,6 +2,7 @@ package garage;
 
 public class Car {
     // TODO: 5: Dodaj pare pól by poćwiczyć skróty i generowanie getterów i setterów
+    // TODO 6: Dziedziczenie i Interfejsy / Polimorfizm
     private String licensePlate;
 
     public Car(String licensePlate) {
